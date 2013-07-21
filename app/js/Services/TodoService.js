0a0c89c096eddde96db367a02404580c9b2eae4c
rhanson <robert.hanson@solutiondesign.com>
2013-07-20 19:21:24 -0500

    todo pretty

--- app/js/Services/TodoService.js
+++ app/js/Services/TodoService.js
@@ -4,7 +4,7 @@ myApp.service("ToDoService", [
         this.ToDoItem = function(date,subject) {
             this.date=date;
             this.subject=subject;
-        }
+        };
 
         this.addItem = function(date, subject) {
             this.ToDoItems.push(new this.ToDoItem(date,subject));
@@ -15,7 +15,6 @@ myApp.service("ToDoService", [
         this.addItem('8/2/2013', "Complete July Expense Report");
 
         this.removeItem = function(item) {
-            debugger
             var index = _.indexOf(this.ToDoItems,item);
             if (index > -1)
                 this.ToDoItems.splice(index,1);
